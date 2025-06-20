b03cb5fa833eb302f42f6dddb8ec78e80ea0fe6e
ckelshaw <codykelshaw11@gmail.com>
2025-06-19 23:28:16 -0600

    Initial Committ. Created project, added dependencies, and started to implement Clerk.

---
+++ client/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+// Importing Publishable Key for Clerk
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+console.log('Clerk Publishable Key:', import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
+if (!PUBLISHABLE_KEY) {
+  console.error('Clerk Publishable Key is not defined. Please set VITE_CLERK_PUBLISHABLE_KEY in your .env file.');
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
